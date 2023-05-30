c8c92e9ad85bee5dee451e6b7415c2dd65dc23e1
Ayushmaan Sharma <106451252+ayushhh231@users.noreply.github.com>
2023-05-30 11:37:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function first(){
+    document.getElementsByTagName("h1")[0].innerText = "#00C4FF";
+    document.getElementsByTagName("h1")[0].style.color = "#00C4FF";
+}
+function second(){
+    document.getElementsByTagName("h1")[0].innerText = "#116D6E";
+    document.getElementsByTagName("h1")[0].style.color = "#116D6E";
+}
+function third(){
+    document.getElementsByTagName("h1")[0].innerText = "#526D82";
+    document.getElementsByTagName("h1")[0].style.color = "#526D82";
+}
+function fourth(){
+    document.getElementsByTagName("h1")[0].innerText = "#FFE569";
+    document.getElementsByTagName("h1")[0].style.color = "#FFE569";
+    
+}
+function fifth(){
+    document.getElementsByTagName("h1")[0].innerText = "#DB005B";
+    document.getElementsByTagName("h1")[0].style.color = "#DB005B";
+}
+function sixth(){
+    document.getElementsByTagName("h1")[0].innerText = "#212A3E";
+    document.getElementsByTagName("h1")[0].style.color = "#212A3E";
+}
+
+function remove(){
+    document.getElementsByTagName("h1")[0].innerText = "";
+}
